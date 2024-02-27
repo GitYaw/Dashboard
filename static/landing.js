53decd4ad3430c885f9d4d910c76b2145dc02cd3
Yaw Mireku <118967566+GitYaw@users.noreply.github.com>
2024-02-27 13:41:46 +0000

    Add image and modify inputs

--- static/landing.js
+++ static/landing.js
@@ -1,5 +1,33 @@
 
+
+window.onload = onPageLoad;
+
 var parameterValues = new Map()
+
+function onPageLoad() {
+    console.log("page loading")
+    createChart()
+}
+
+function createChart() {
+    
+    var initialData = {
+        labels: ["Labor", "Materials", "Equipment", "Other"],
+        datasets: [{
+            data: [10, 20, 30, 40],
+            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4CAF50"],
+        }]
+    };
+
+    var context = document.getElementById('expensesChart').getContext('2d');
+
+    var expensesChart = new Chart(context, {
+        type: 'doughnut',
+        data: initialData,
+        options: {
+        }
+    });
+}
 
 function changeBioreactorType() {
     // change diagram
@@ -24,14 +52,36 @@ function changeBioreactorType() {
     bioreactorLabel.innerHTML = "Flow Diagram: " + bioreactorName;	
 }
 
-function updateParameter(parameter, value, fromRangeInput) {
-    parameterValues.set(parameter, value)
+// function updateParameter(parameter, value, fromRangeInput) {
+//     parameterValues.set(parameter, value)
 
-    if (fromRangeInput) {
-        var numberInput = document.getElementById(parameter)
-        numberInput.value = value
+//     if (fromRangeInput) {
+//         var numberInput = document.getElementById(parameter)
+//         numberInput.value = value
+//     } else {
+//         var rangeInput = document.getElementById(parameter + "Slider")
+//         rangeInput.value = value
+//     }
+// }
+
+function updateChart() {
+    var updatedData = {
+        labels: ["Labor", "Materials", "Equipment", "Other"],
+        datasets: [{
+            data: [40, 30, 20, 10],
+            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4CAF50"],
+        },{
+                      data: [10, 30, 20, 40],
+                      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#4CAF50"],
+                  }]
+    };
+
+    var expensesChart = Chart.getChart('expensesChart');
+
+    if (expensesChart) {
+        expensesChart.data.datasets = updatedData.datasets;
+        expensesChart.update();
     } else {
-        var rangeInput = document.getElementById(parameter + "Slider")
-        rangeInput.value = value
+        createChart();
     }
 }
